beff5e210d606e74f8236500ed26d1f190209ffd
erik savage <erik.k.savage@gmail.com>
2022-01-30 17:55:32 -0800

    refactors disconnect and submit

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 3000;
 const server = socketio(PORT);
 const wurd = server.of('/wurd');
 
-const validWord = require('./dictionary');
+//this would be to use dictionary API to validate word
+// const validWord = require('./dictionary');
 
 const queue = {
   submissions: {},
@@ -19,7 +20,6 @@ const queue = {
   playAgainCount: 0,
   addSubmission: function (payload) {
     // function to add submissions to queue
-    // adds id: word pair
     this.submissions[payload.userName] = payload; // placeholder
     // console.log(this.submissions.payload);
   },
@@ -48,8 +48,6 @@ wurd.on('connection', (socket) => {
     queue.playerCount += 1; // increase player count when new players join
     console.log('playercount', queue.playerCount);
     readyCheck();
-    // socket.join(clientId);
-    // wurd.emit('join', clientId);
   });
 
   socket.on('newround', payload => {
@@ -63,8 +61,11 @@ wurd.on('connection', (socket) => {
     }
   });
 
-  socket.on('disconnect', () => {
+  socket.on('disconnect', (player) => {
     queue.playerCount -= 1;
+    console.log(player);
+    console.log('someone disconnected');
+    wurd.emit('gamestatus', 'Player 2 Disconnected, Waiting for Additional Player');
   });
 
   // receive new word back from client
@@ -81,6 +82,7 @@ wurd.on('connection', (socket) => {
       let winner;
       let longest = 0;
       let word;
+      let result;
       Object.keys(queue.submissions).forEach((key) => {
         let answer = queue.submissions[key].answer;
         let currentPlayer = queue.submissions[key].userName;
@@ -93,32 +95,18 @@ wurd.on('connection', (socket) => {
           winner = 'TIED';
         }
 
-
       });
       if (winner === 'TIED') {
-        console.log(`You Tied`);  
+        result = `You Tied`;  
       } else {
-        console.log(`${winner} WINS with ${word}!`);
+        result = `${winner} WINS with ${word}!`;
       }
-
+      console.log(result);
       queue.removeSubmissions();
       console.log('SUBMISSIONS', queue.submissions);
-      wurd.emit('playagain');
+      wurd.emit('playagain', result);
     }  
   });
-      // check how many valid words, sort by length if multiple are valid
-    //   if (validWords.length === 0) {
-    //     payload = 'You\'re all wrong';
-    //   } else if (validWords.length === 1) {
-    //     payload = `The winner is player ${validWords[0][0]} with the word ${validWords[0][1]}`; // display id and winning word
-    //   } else {
-    //     // determine winner by sorting wurd lengths if only 2 users
-    //     payload = checkWurdLength(validWords);
-    //   }
-    // }
-    // //validate for each word send check via dictionaryAPI, if valid, longest length wins, if same length, tie.
-    // wurd.emit('winner', payload);
-  // });
 
   function readyCheck() {
     console.log('made it into readyCHeck function');
@@ -129,42 +117,7 @@ wurd.on('connection', (socket) => {
       socket.emit('gamestatus', 'Waiting for additional player');
     }
   }
-
-  // check wurd lengths to determin winner if only 2 players
-  function checkWurdLength(arr) {
-    console.log('Valid Word Array: ', arr);
-    let winner;
-
-    if (arr[0][1] > arr[1][1].length) {
-      winner = `The winner is ${arr[0][0]}!`; // winner userName
-    } else if (arr[0][1].length < arr[1][3].length) {
-      winner = `The winner is ${arr[1][3]}!`; // winner userName
-    } else {
-      winner = 'It\'s a draw!';
-    }
-    console.log('Winner: ', winner);
-    return winner;
-  }
 });
-
-
-
-
-// function sortWurdLengths(arr) {
-//   let sortedWords = arr.sort((a, b) => {
-//     return a.wurd.length - b.wurd.length;  // double check
-//   });
-//   let winners = [];
-//   // we know the first word is winner
-//   winners.push(sortedWords[0].id);
-//   // check for other winners
-//   // start checking at second word
-//   for (let i = 1; i < sortedWords.length; i += 1) {
-//     if (sortedWords[i][1].length === sortedWords[0][1].length) {
-//       winners.push(sortedWords[i][0]);
-//     }
-//   }
-//   return winners; // right now just returns list of winner ids
 
 
 
